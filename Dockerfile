9ec4674ebbee14030686f2edf443b13b5601cb3b
Oleksiy.Dovzhanitsya <oleksiy@brkt.com>
2017-09-22 11:56:29 +0300

    Downgraded  swarm plugin and refactored run flow

--- Dockerfile
+++ Dockerfile
@@ -5,16 +5,17 @@ MAINTAINER  Oleksiy Dovzhanitsya <oleksdovz@gmail.com>
 #
 ENV LANG en_US.UTF-8
 ENV LANGUAGE en_US.UTF-8
+ENV JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64/jre
 #
 RUN apt update && \
     apt install -y openjdk-8-jre && \
     apt clean && \
     mkdir -pv  /jenkins /jenkins/plugins
 #
-ADD https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/3.4/swarm-client-3.4.jar /jenkins/plugins/
+ADD https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/3.3/swarm-client-3.3.jar /jenkins/plugins/swarm-client.jar
 #
 ADD swarm-agent.sh /usr/bin/swarm-agent.sh
 #
 RUN chmod 777 /usr/bin/swarm-agent.sh
 #
-ENTRYPOINT /usr/bin/swarm-agent.sh
+# ENTRYPOINT /usr/bin/swarm-agent.sh
--- swarm-agent.sh
+++ swarm-agent.sh
@@ -1,31 +1,35 @@
 #!/bin/bash -x
-java_exe='/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java'
-swarm_jar='/jenkins/plugins/swarm-client-3.4.jar'
+cat << EOF > /tmp/swarm-settings
+${AUTODISCOVERYADDRESS:+-autoDiscoveryAddress $AUTODISCOVERYADDRESS}
+${CANDIDATETAG:+-candidateTag $CANDIDATETAG}
+${DELETEEXISTINGCLIENTS:+-deleteExistingClients $DELETEEXISTINGCLIENTS}
+${DESCRIPTION:+-description $DESCRIPTION}
+${DISABLESSLVERIFICATION:+-disableSslVerification $DISABLESSLVERIFICATION}
+${DISABLECLIENTSUNIQUEID:+-disableClientsUniqueId $DISABLECLIENTSUNIQUEID}
+${EXECUTORS:+-executors $EXECUTORS}
+${FSROOT:+-fsroot $FSROOT}
+${LABELS:+-labels $LABELS}
+${LABELSFILE:+-labelsFile $LABELSFILE}
+${LOGFILE:+-logFile $LOGFILE}
+${MASTER:+-master "$MASTER"}
+${MAXRETRYINTERVAL:+-maxRetryInterval $MAXRETRYINTERVAL}
+${MODE:+-mode $MODE}
+${NAME:+-name $NAME}
+${NORETRYAFTERCONNECTED:+-noRetryAfterConnected $NORETRYAFTERCONNECTED}
+${PASSWORDENVVARIABLE:+-passwordEnvVariable $PASSWORDENVVARIABLE}
+${RETRY:+-retry $RETRY}
+${RETRYBACKOFFSTRATEGY:+-retryBackOffStrategy $RETRYBACKOFFSTRATEGY}
+${RETRYINTERVAL:+-retryInterval $RETRYINTERVAL}
+${SHOWHOSTNAME:+-showHostName $SHOWHOSTNAME}
+${SSLFINGERPRINTS:+-sslFingerprints $SSLFINGERPRINTS}
+${T:+-t $T}
+${TUNNEL:+-tunnel $TUNNEL}
+${USERNAME:+-username $USERNAME}
+${PASSWORD:+-password  $PASSWORD}
+EOF
 
-$java_exe -jar $swarm_jar  \
-${AUTODISCOVERYADDRESS:+-autoDiscoveryAddress $AUTODISCOVERYADDRESS} \
-${CANDIDATETAG:+-candidateTag $CANDIDATETAG} \
-${DELETEEXISTINGCLIENTS:-deleteExistingClients $DELETEEXISTINGCLIENTS} \
-${DESCRIPTION:-description $DESCRIPTION} \
-${DISABLECLIENTSUNIQUEID:-disableClientsUniqueId $DISABLECLIENTSUNIQUEID} \
-${DISABLESSLVERIFICATION:-disableSslVerification $DISABLESSLVERIFICATION} \
-${EXECUTORS:-executors $EXECUTORS} \
-${FSROOT:-fsroot $FSROOT} \
-${LABELS:-labels $LABELS} \
-${LABELSFILE:-labelsFile $LABELSFILE} \
-${LOGFILE:-logFile $LOGFILE} \
-${MASTER:-master $MASTER} \
-${MAXRETRYINTERVAL:-maxRetryInterval $MAXRETRYINTERVAL} \
-${MODE:-mode $MODE} \
-${NAME:-name $NAME} \
-${NORETRYAFTERCONNECTED:-noRetryAfterConnected $NORETRYAFTERCONNECTED} \
-${PASSWORD:-password  $PASSWORD} \
-${PASSWORDENVVARIABLE:-passwordEnvVariable $PASSWORDENVVARIABLE} \
-${RETRY:-retry $RETRY} \
-${RETRYBACKOFFSTRATEGY:-retryBackOffStrategy $RETRYBACKOFFSTRATEGY} \
-${RETRYINTERVAL:-retryInterval $RETRYINTERVAL} \
-${SHOWHOSTNAME:-showHostName $SHOWHOSTNAME} \
-${SSLFINGERPRINTS:-sslFingerprints $SSLFINGERPRINTS} \
-${T:-t $T} \
-${TUNNEL:-tunnel $TUNNEL} \
-${USERNAME:-username $USERNAME} \
+swarm_settings="$(cat /tmp/swarm-settings)"
+java_exe='/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java'
+swarm_jar='/jenkins/plugins/swarm-client.jar'
+cd  /jenkins
+$java_exe -jar $swarm_jar  $swarm_settings
